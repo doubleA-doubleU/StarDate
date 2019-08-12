ba90b96b29b59b1eba97e2b528f4dccd4dd5380a
weatherman03 <aaron.m.weatherly@gmail.com>
2019-08-13 01:50:57 +0800

    Android Updates
    Widget now updating every 60 seconds with alarm manager

--- Android/DeciWatch/app/src/main/java/aaron/deciwatch/NewAppWidget.java
+++ Android/DeciWatch/app/src/main/java/aaron/deciwatch/NewAppWidget.java
@@ -1,33 +1,60 @@
 package aaron.deciwatch;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 import java.util.Calendar;
 import java.util.Locale;
+
+import static android.appwidget.AppWidgetManager.ACTION_APPWIDGET_UPDATE;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class NewAppWidget extends AppWidgetProvider {
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
-        int h = Calendar.getInstance().getTime().getHours();
-        int m = Calendar.getInstance().getTime().getMinutes();
-        int s = Calendar.getInstance().getTime().getSeconds();
-        int sec = h*3600 + m*60 + s;
-        double dec = sec/0.864;
-        int dh = (int) (dec/10000);
-        int dm = (int) ((dec - dh*10000) / 100);
-        int ds = (int) (dec - dh*10000 - dm*100);
-        CharSequence widgetText = String.format(Locale.getDefault(),
-                "%02d:%02d:%02d\n%02d:%02d:%02d", h, m, s, dh, dm, ds);
 
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        if (ACTION_APPWIDGET_UPDATE.equals(intent.getAction())) {
+            ComponentName thisAppWidget = new ComponentName(context.getPackageName(), getClass().getName());
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            int ids[] = appWidgetManager.getAppWidgetIds(thisAppWidget);
+            for (int appWidgetID : ids) {
+                updateAppWidget(context, appWidgetManager, appWidgetID);
+
+            }
+        }
+    }
+
+    private PendingIntent createClockTickIntent(Context context) {
+        Intent intent = new Intent(ACTION_APPWIDGET_UPDATE);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        return pendingIntent;
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(createClockTickIntent(context));
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.add(Calendar.SECOND, 1);
+        alarmManager.setRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), 1000, createClockTickIntent(context));
     }
 
     @Override
@@ -38,14 +65,22 @@ public class NewAppWidget extends AppWidgetProvider {
         }
     }
 
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
+    public static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        int h = Calendar.getInstance().getTime().getHours();
+        int m = Calendar.getInstance().getTime().getMinutes();
+        int s = Calendar.getInstance().getTime().getSeconds();
+        int sec = h * 3600 + m * 60 + s;
+        double dec = sec / 0.864;
+        int dh = (int) (dec / 10000);
+        int dm = (int) ((dec - dh * 10000) / 100);
+        int ds = (int) (dec - dh * 10000 - dm * 100);
+        CharSequence widgetText = String.format(Locale.getDefault(),
+                "%02d:%02d:%02d\n%02d:%02d:%02d", h, m, s, dh, dm, ds);
 
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 }
-
