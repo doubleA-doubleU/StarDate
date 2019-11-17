6ba5b09d4b476f321ffe7871cebff3ac08e98a45
weatherman03 <aaron.m.weatherly@gmail.com>
2019-11-16 23:36:33 -0500

    Added StarDate mod for Windows

---
+++ Windows/StarDate/src/StarDate.js
@@ -0,0 +1,31 @@
+function init() {
+	// format gadget size
+	document.body.style.width = 195;
+	document.body.style.height = 60;
+	
+	// run the main function
+	main();
+}
+
+function main() {
+	// get the current time
+	var dayZero = new Date("04/12/1961");
+	var date = new Date();
+	var d = Math.floor((date.getTime() - dayZero.getTime())/86400000);
+	var h = date.getHours();
+	var m = date.getMinutes();
+	var s = date.getSeconds();
+	
+	// convert time to seconds, then "dours/dinutes/deconds"
+	var sec = h*3600 + m*60 + s;
+	var dec = Math.floor(sec/0.864); // added Math.floor
+	
+	// construct StarDate as string
+	var sdate = d + "." + dec;
+	
+	// make variable available to html
+	document.getElementById("sdate").innerHTML = sdate; // added
+	
+	// update 9 times per decond
+	setTimeout(main,96);
+}
